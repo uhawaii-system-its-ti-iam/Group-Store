e205a69fa4a08f2735441d15787941f76167141a
r-robles <rfrobles4@gmail.com>
2017-09-22 11:18:42 -1000

    Add comments to group configuration controller

--- src/main/resources/static/javascript/store.controller.group-configuration.js
+++ src/main/resources/static/javascript/store.controller.group-configuration.js
@@ -1,6 +1,7 @@
 (function() {
   function GroupConfigurationController($scope, CartService, $uibModalInstance) {
 
+    /** The possible ways for groups to be configured. The properties are just the number of groups selected */
     $scope.vennOptions = {
       2: [
         { name: 'A ∪ B', description: 'People in either group A or group B', image: 'a-union-b.png' },
@@ -16,32 +17,58 @@
       ]
     };
 
+    /** The configuration selected by the user */
     $scope.selected;
 
+    /**
+     * @returns {object[]} an array of groups in the user's cart
+     */
     $scope.getGroupsInCart = function() {
       return CartService.getGroupsInCart();
     };
 
+    /**
+     * @returns {number} the number of groups in the user's cart
+     */
     $scope.getAmountInCart = function() {
       return CartService.getAmountInCart();
     };
 
+    /**
+     * Closes the modal.
+     */
     $scope.closeModal = function() {
       $uibModalInstance.dismiss();
     };
 
+    /**
+     * Stores the group configuration option selected by the user.
+     * @param {object} option - the option selected by the user
+     */
     $scope.setSelected = function(option) {
-      return $scope.selected = option;
+      $scope.selected = option;
     };
 
+    /**
+     * Returns the configuration selected by the user. If the user has not made a selection, it returns the an
+     * object containing the default venn diagram.
+     * @returns {object} the configuration selected by the user, or the default venn diagram
+     */
     $scope.getSelected = function() {
       if (!!$scope.selected) {
         return $scope.selected;
-      } else {
-        return $scope.vennOptions[$scope.getAmountInCart()].slice(-1)[0];
       }
+      // Returns the default venn diagram object, which is stored in the last index of each venn diagram option
+      return $scope.vennOptions[$scope.getAmountInCart()].slice(-1)[0];
     };
 
+    /**
+     * Returns the letter corresponding to an integer's position in the alphabet.
+     * @example 0 returns 'A'
+     * @example 25 returns 'Z'
+     * @param {number} index - the integer position in the alphabet
+     * @returns {string} the character corresponding to index
+     */
     $scope.getCorrespondingLetter = function(index) {
       return String.fromCharCode(index + 65);
     };
