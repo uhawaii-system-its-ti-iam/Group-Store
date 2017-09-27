b40b4e9576a4c9df767abf296813f88cafe01909
r-robles <rfrobles4@gmail.com>
2017-09-27 12:17:28 -1000

    Return GET request to allow promise chains

--- src/main/resources/static/javascript/store.app.service.js
+++ src/main/resources/static/javascript/store.app.service.js
@@ -2,7 +2,7 @@
   storeApp.factory('dataProvider', function($http) {
     return {
       loadData: function(callback, url) {
-        $http.get(encodeURI(url))
+        return $http.get(encodeURI(url))
             .then(callback, function(response) {
               console.log('Error in dataProvider; response: ', response);
             });
